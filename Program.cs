af824ace82b80f02e8c08db046973b1e939d9d5e
Gustavo Malisse <gostavomalisse@gmail.com>
2024-10-23 16:27:19 -0300

    started the project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+            using (var client = new HttpClient()) {
+                var endpoint = new Uri("https://api.github.com/users/gmalisse/events");
+
+               
+                var result = client.GetAsync(endpoint).Result;
+                var json = result.Content.ReadAsStringAsync().Result;
+            }
+        }
+    }
+}
